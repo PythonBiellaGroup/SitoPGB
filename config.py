d3bb13e848cbf9059f8b9cae1bbe182d9cb2e2a4
Burlesco70 <mario.nardi.70@gmail.com>
2020-12-03 18:19:35 +0100

    sistemazione keys

--- config.py
+++ config.py
@@ -15,8 +15,8 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get("SECRET_KEY") or "supersupersupersecretkey"
     # Supportata recaptcha v.2
-    RECAPTCHA_PUBLIC_KEY = "6LcxL_UZAAAAAGOEzIQRc_zPaNQVub35WiR1utkE"
-    RECAPTCHA_PRIVATE_KEY = "6LcxL_UZAAAAAJn7gapJSqOPuB_MT13-VoBCPUmP"
+    RECAPTCHA_PUBLIC_KEY = os.environ.get("RECAPTCHA_PUBLIC_KEY")
+    RECAPTCHA_PRIVATE_KEY = os.environ.get("RECAPTCHA_PRIVATE_KEY")
     # Mail
     MAIL_SERVER = os.environ.get("MAIL_SERVER", "smtp.googlemail.com")
     MAIL_PORT = int(os.environ.get("MAIL_PORT", "587"))
@@ -38,33 +38,17 @@ class Config(object):
 
 
 class ProdConfig(Config):
+    # Db
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(
         basedir, "db", "data.sqlite"
     ) or os.environ.get("SQLALCHEMY_DATABASE_URI")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "supersupersupersecretkey"
-
-    # Supportata recaptcha v.2
-    RECAPTCHA_PUBLIC_KEY = os.environ.get("RECAPTCHA_PUBLIC_KEY")
-    RECAPTCHA_PRIVATE_KEY = os.environ.get("RECAPTCHA_PRIVATE_KEY")
     # Mail
     MAIL_SERVER = os.environ.get("MAIL_SERVER")
     MAIL_PORT = os.environ.get("MAIL_PORT")
     MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS") in ["true", "on", "1"]
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-
     # Costanti usati nelle mail registrazione utenti
     PBG_MAIL_SUBJECT_PREFIX = os.environ.get("PBG_MAIL_SUBJECT_PREFIX")
     PBG_MAIL_SENDER = os.environ.get("PBG_MAIL_SENDER")
-
-    # Definizione email dell'utente ADMIN iniziale
-    PBG_ADMIN = os.environ.get("PBG_ADMIN")
-
-    # Per la paginazione
-    PBG_COMMENTS_PER_PAGE = 5
-    PBG_POSTS_PER_PAGE = 5
-
     TESTING = False
     DEBUG = False
 
@@ -72,15 +56,9 @@ class ProdConfig(Config):
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "db", "data.sqlite")
     DEBUG = True
-    # Settings per usare https://mailtrap.io/ - Registrati e cambia con i tuoi dati
-    MAIL_SERVER = "smtp.mailtrap.io"
-    MAIL_USERNAME = "9a973adb59007f"
-    MAIL_PASSWORD = "4f5fd306232d45"
-    MAIL_PORT = 2525
+    # Consiglio di usare https://mailtrap.io/ - Registrati
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
-    PBG_ADMIN = "burlesco70@test.it"
-
 
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(
